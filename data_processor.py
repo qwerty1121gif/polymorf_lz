e6e0c3170b8d02fc21fd25f68ef05c2ac860a572
qwerty1121gif <Vladimirstalin@tutamail.com>
2025-04-22 20:03:37 +0300

    done

--- data_processor.py
+++ data_processor.py
@@ -7,20 +7,20 @@ class DataProcessor:
         self.removed_duplicates = 0
 
     def split_by_city(self):
-        """Разделяет датасет на Минск и остальные города."""
+        """Разделяет датасет на Минск и другие города"""
         minsk = self.df[self.df['Место оплаты'] == 'Минск']
         other = self.df[self.df['Место оплаты'] != 'Минск']
         return minsk, other
     
     def save_split_datasets(self):
-        """Сохраняет разделённые датасеты в CSV-файлы."""
+        """Сохраняет разделённые датасеты в csv-файлы"""
         minsk, other = self.split_by_city()
         minsk.to_csv('minsk.csv', index=False)
         other.to_csv('other_cities.csv', index=False)
         print("Файлы сохранены: minsk.csv и other_cities.csv")
         
     def __neg__(self):
-        """Унарный оператор '-' для удаления дубликатов."""
+        """Унарный оператор '-' для удаления дубликатов"""
         initial_rows = len(self.df)
         self.df.drop_duplicates(inplace=True)
         self.removed_duplicates = initial_rows - len(self.df)
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     # Инициализация обработчика данных 
     processor = DataProcessor('var5.csv') 
 
-    # Удаление дубликатов через унарный оператор 
+    # Удаление повторок через унарный оператор 
     removed_duplicates = -processor 
     print(f"Количество удалённых дубликатов: {removed_duplicates}") 
     
-    # Сохранение разделённых датасетов 
+    # Сохранение разделенных датасетов 
     processor.save_split_datasets() 
